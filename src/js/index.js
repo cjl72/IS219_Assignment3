3c256269d49b69205bcd59141db65ddac32a5f2f
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-04-12 11:17:11 -0400

    Task: refactored for different data

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,12 @@
 (function ($) {
-    $('#citiesTable').DataTable( {
-        "ajax": "http://localhost:9080/api/v1/cities",
+    $('#bioTable').DataTable( {
+        "ajax": "http://localhost:9080/api/v1/bio",
         "columns": [
-            { "data": "id" },
-            { "data": "fldName" },
-            { "data": "fldLat" },
-            { "data": "fldLong" },
-            { "data": "fldCountry" },
-            { "data": "fldAbbreviation" },
-            { "data": "fldCapitalStatus" },
-            { "data": "fldPopulation" }
-
+            { "data": "Name" },
+            { "data": "Sex" },
+            { "data": "Age" },
+            { "data": "Height_in" },
+            { "data": "Weight_lbs" }
         ]
     } );
 
